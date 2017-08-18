3237ee64c944c4fb0b17db5712bd3a21823340e3
Kamil <blueflamesxcode@gmail.com>
2017-08-18 12:44:40 +0200

    Add implemented TodoItem class

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,44 @@
 /**
  * Class representing a single todo item.
- * 
+ *
  */
+
+
 public class TodoItem{
+    private boolean isDone;
+    private String taskDetails;
+
+    public TodoItem(String aTask)
+    {
+        this.isDone = false;
+        this.taskDetails = aTask;
+
+    }
+
+    public void markTaskAsDone()
+    {
+        this.isDone = true;
+
+    }
+
+    public boolean getTaskStatus()
+    {
+        return this.isDone;
+    }
+
+    public String toString()
+    {
+        if (this.isDone)
+            return "[x] " + this.taskDetails;
+
+        else if (!this.isDone)
+            return "[ ] " + this.taskDetails;
+
+        return null;
+
+    }
+
+
+
 
 }
